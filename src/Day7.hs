83958c7cefd87b410db340340f0cfcf5613e9734
Robbin Ploeger <robbin.ploeger@bettyblocks.com>
2024-12-07 08:49:59 +0000

    day 7

--- src/Day7.hs
+++ src/Day7.hs
@@ -1,56 +1,39 @@
 module Day7
   ( part1,
     part2,
-    parseLine,
   )
 where
 
-import Control.Monad
-import Data.Function
-import qualified Data.HashMap.Strict as M
-import Data.List (nub)
 import Data.List.Split
 
-type Hmap = M.HashMap String [(String, Int)]
+type Equation = (Int, [Int])
 
 part1 :: String -> Int
-part1 input = length $ nub $ concatMap (\key -> dfs key hm []) (M.keys hm)
-  where
-    hm = parse input
+part1 input = sum . map fst . filter (`isValid` 0) $ parse input
 
-parse :: String -> Hmap
-parse input = M.fromList $ map parseLine $ lines input
+parse :: String -> [Equation]
+parse input = map line $ lines input
 
-parseLine :: String -> (String, [(String, Int)])
-parseLine line = (key, map parseValue valueChunks)
-  where
-    kv = line & init & splitOn " contain "
-    key = kv & head & words & take 2 & unwords
-    valueChunks = splitOn ", " $ last kv
+line :: String -> Equation
+line str =
+  let parts = splitOn ": " str
+      nums = words . last $ parts
+   in (read . head $ parts, map read nums)
 
-parseValue :: String -> (String, Int)
-parseValue ('n' : 'o' : _) = ("Nothing", 0)
-parseValue value = (key, count)
+isValid :: Equation -> Int -> Bool
+isValid (sum, []) total = total == sum
+isValid (sum, x : xs) total = next (x + total) || next (x * total)
   where
-    w = words value
-    key = unwords . init . drop 1 $ w
-    count = read (head w) :: Int
-
-dfs :: String -> Hmap -> [String] -> [String]
-dfs "Nothing" _ _ = []
-dfs "shiny gold" _ acc = acc
-dfs key hmap acc = concatMap (\next -> dfs (fst next) hmap (key : acc)) bags
-  where
-    bags = hmap M.! key
+    next = isValid (sum, xs)
 
 part2 :: String -> Int
-part2 input = dfs2 hm ("shiny gold", 1) - 1
-  where
-    hm = parse input
+part2 input = sum . map fst . filter (`isValidConcat` 0) $ parse input
 
-dfs2 :: Hmap -> (String, Int) -> Int
-dfs2 _ ("Nothing", _) = 0
-dfs2 hmap (key, count) = (count * sum (map (dfs2 hmap) bags)) + count
+isValidConcat :: Equation -> Int -> Bool
+isValidConcat (sum, []) total = total == sum
+isValidConcat (sum, x : xs) total = next (x + total) || next (x * total) || next (concatInt total x)
   where
-    bags = hmap M.! key
-    next (nextKey, count) = dfs2 hmap nextKey
+    next = isValidConcat (sum, xs)
+
+concatInt :: Int -> Int -> Int
+concatInt a b = read $ show a <> show b
--- test/Day7Spec.hs
+++ test/Day7Spec.hs
@@ -7,34 +7,20 @@ input :: String
 input =
   init $
     unlines
-      [ "light red bags contain 1 bright white bag, 2 muted yellow bags.",
-        "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
-        "bright white bags contain 1 shiny gold bag.",
-        "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
-        "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
-        "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
-        "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
-        "faded blue bags contain no other bags.",
-        "dotted black bags contain no other bags."
-      ]
-
-input2 :: String
-input2 =
-  init $
-    unlines
-      [ "shiny gold bags contain 2 dark red bags.",
-        "dark red bags contain 2 dark orange bags.",
-        "dark orange bags contain 2 dark yellow bags.",
-        "dark yellow bags contain 2 dark green bags.",
-        "dark green bags contain 2 dark blue bags.",
-        "dark blue bags contain 2 dark violet bags.",
-        "dark violet bags contain no other bags."
+      [ "190: 10 19",
+        "3267: 81 40 27",
+        "83: 17 5",
+        "156: 15 6",
+        "7290: 6 8 6 15",
+        "161011: 16 10 13",
+        "192: 17 8 14",
+        "21037: 9 7 18 13",
+        "292: 11 6 16 20"
       ]
 
 spec :: Spec
 spec = do
   it "part 1" $ do
-    part1 input `shouldBe` 4
+    part1 input `shouldBe` 3749
   it "part 2" $ do
-    part2 input `shouldBe` 32
-    part2 input2 `shouldBe` 126
+    part2 input `shouldBe` 11387
